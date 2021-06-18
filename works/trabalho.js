9a5290e9408fb20d91381f757d743701ea3ebd75
andrediasn <andredias@ice.ufjf.br>
2021-06-17 21:01:26 -0300

    Update

--- works/trabalho.js
+++ works/trabalho.js
@@ -7,113 +7,116 @@ import { initRenderer,
         InfoBox,
         onWindowResize,
         degreesToRadians, 
-        createGroundPlaneWired} from '../libs/util/util.js'
+        createGroundPlaneWired,
+        initDefaultBasicLight} from '../libs/util/util.js'
 
 var stats = new Stats() // To show FPS information
 var scene = new THREE.Scene() // Create main scene
 var renderer = initRenderer() // View function in util/utils
-//var camera = initCamera(new THREE.Vector3(0, -60, 40)) // Init camera in this position
 
-// Enable mouse rotation, pan, zoom etc.
-// var trackballControls = new TrackballControls(camera, renderer.domElement)
-
-// ---------------------------Código vem aqui---------------------------------- //
 
 // Variáveis iniciais
 var mat4 = new THREE.Matrix4()
-var animation = true; // control if animation is on or of
+const c = 0.6 // Constante para modificar o tamanho do avião
 
-// Constante para modificar o tamanho do avião
-const c = 0.6
+// Plano e iluminação
+var plane = createGroundPlaneWired(2000, 2000, 50, 50)
+//plane.rotateX(degreesToRadians(-90));
+initDefaultBasicLight(scene)
 
 // ----------------- Criação dos polígonos ----------------- //
 
 // Esfera que é a base de tudo
 var esferaHeliceGeo = new THREE.SphereGeometry(2 * c, 20, 20)
-var esferaHeliceMat = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+var esferaHeliceMat = new THREE.MeshPhongMaterial({ color: 0xff0000 })
 var esferaHelice = new THREE.Mesh(esferaHeliceGeo, esferaHeliceMat)
 
 // Cone da hélice
 var coneHeliceGeo = new THREE.CylinderGeometry(0.1 * c, 1.3 * c, 1.5 * c, 12)
-var coneHeliceMat = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+var coneHeliceMat = new THREE.MeshPhongMaterial({ color: 0xff0000 })
 var coneHelice = new THREE.Mesh(coneHeliceGeo, coneHeliceMat)
 
 // Cilindro que rotaciona na hélice
 var cylHeliceGeo = new THREE.CylinderGeometry(0.3 * c, 0.3 * c, 20 * c, 10)
-var cylHeliceMat = new THREE.MeshBasicMaterial({ color: 0xffffff })
+var cylHeliceMat = new THREE.MeshPhongMaterial({ color: 0xffffff })
 var cylHelice = new THREE.Mesh(cylHeliceGeo, cylHeliceMat)
 
 // Cilindros do corpo do avião
 var cyl1corpoGeo = new THREE.CylinderGeometry(1.9 * c, 4.8 * c, 6 * c, 16)
-var cyl1corpoMat = new THREE.MeshBasicMaterial({ color: 0xd3d3d3 })
+var cyl1corpoMat = new THREE.MeshPhongMaterial({ color: 0xd3d3d3 })
 var cyl1corpo = new THREE.Mesh(cyl1corpoGeo, cyl1corpoMat)
 
 var cyl2corpoGeo = new THREE.CylinderGeometry(4.8 * c, 7 * c, 10 * c, 18)
-var cyl2corpoMat = new THREE.MeshBasicMaterial({ color: 0xd3d3d3 })
+var cyl2corpoMat = new THREE.MeshPhongMaterial({ color: 0xd3d3d3 })
 var cyl2corpo = new THREE.Mesh(cyl2corpoGeo, cyl2corpoMat)
 
 var cyl3corpoGeo = new THREE.CylinderGeometry(7 * c, 8.5 * c, 10 * c, 18)
-var cyl3corpoMat = new THREE.MeshBasicMaterial({ color: 0xd3d3d3 })
+var cyl3corpoMat = new THREE.MeshPhongMaterial({ color: 0xd3d3d3 })
 var cyl3corpo = new THREE.Mesh(cyl3corpoGeo, cyl3corpoMat)
 
 var cyl4corpoGeo = new THREE.CylinderGeometry(8.5 * c, 8.5 * c, 30 * c, 18)
-var cyl4corpoMat = new THREE.MeshBasicMaterial({ color: 0xd3d3d3 })
+var cyl4corpoMat = new THREE.MeshPhongMaterial({ color: 0xd3d3d3 })
 var cyl4corpo = new THREE.Mesh(cyl4corpoGeo, cyl4corpoMat)
 
 var cyl5corpoGeo = new THREE.CylinderGeometry(8.5 * c, 6 * c, 12 * c, 24)
-var cyl5corpoMat = new THREE.MeshBasicMaterial({ color: 0xd3d3d3 })
+var cyl5corpoMat = new THREE.MeshPhongMaterial({ color: 0xd3d3d3 })
 var cyl5corpo = new THREE.Mesh(cyl5corpoGeo, cyl5corpoMat)
 
 var cyl6corpoGeo = new THREE.CylinderGeometry(6 * c, 3 * c, 9 * c, 18)
-var cyl6corpoMat = new THREE.MeshBasicMaterial({ color: 0xd3d3d3 })
+var cyl6corpoMat = new THREE.MeshPhongMaterial({ color: 0xd3d3d3 })
 var cyl6corpo = new THREE.Mesh(cyl6corpoGeo, cyl6corpoMat)
 
 var cyl7corpoGeo = new THREE.CylinderGeometry(3 * c, 2.2 * c, 7 * c, 18)
-var cyl7corpoMat = new THREE.MeshBasicMaterial({ color: 0xd3d3d3 })
+var cyl7corpoMat = new THREE.MeshPhongMaterial({ color: 0xd3d3d3 })
 var cyl7corpo = new THREE.Mesh(cyl7corpoGeo, cyl7corpoMat)
 
 // Asas
 var asaGeo = new THREE.BoxGeometry(30 * c, 10 * c, 1.2 * c)
-var asaMat = new THREE.MeshBasicMaterial({ color: 0xc4c4c4 })
+var asaMat = new THREE.MeshPhongMaterial({ color: 0xc4c4c4 })
 
 var asa1 = new THREE.Mesh(asaGeo, asaMat)
 var asa2 = new THREE.Mesh(asaGeo, asaMat)
 
 // Esfera do leme
 var esferaLemeGeo = new THREE.SphereGeometry(2.2 * c, 20, 20)
-var esferaLemeMat = new THREE.MeshBasicMaterial({ color: 0xff00000 })
+var esferaLemeMat = new THREE.MeshPhongMaterial({ color: 0xff00000 })
 var esferaLeme = new THREE.Mesh(esferaLemeGeo, esferaLemeMat)
 
 // Cilindro do leme
 var cylLemeGeo = new THREE.CylinderGeometry(1.8 * c, 1.0 * c, 2 * c, 10)
-var cylLemeMat = new THREE.MeshBasicMaterial({ color: 0xff00000 })
+var cylLemeMat = new THREE.MeshPhongMaterial({ color: 0xff00000 })
 var cylLeme = new THREE.Mesh(cylLemeGeo, cylLemeMat)
 
 // Retângulo na vertical do leme
 var retVerLemeGeo = new THREE.BoxGeometry(0.4 * c, 8 * c, 2.4 * c)
-var retVerLemeMat = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+var retVerLemeMat = new THREE.MeshPhongMaterial({ color: 0xff0000 })
 var retVerLeme = new THREE.Mesh(retVerLemeGeo, retVerLemeMat)
 
 // Retângulo na horizontal do leme
 var retHorLemeGeo = new THREE.BoxGeometry(16 * c, 3.0 * c, 0.6 * c)
-var retHorLemeMat = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+var retHorLemeMat = new THREE.MeshPhongMaterial({ color: 0xff0000 })
 var retHorLeme = new THREE.Mesh(retHorLemeGeo, retHorLemeMat)
 
 // Cabine do avião
 var cylCabineGeo = new THREE.CylinderGeometry(3 * c, 3 * c, 4.5 * c, 30)
 var sCabineGeo = new THREE.SphereGeometry(3.0 * c, 20, 20)
 
-var cylCabineMat = new THREE.MeshBasicMaterial({ color: 0x656565 })
-var sCabineMat = new THREE.MeshBasicMaterial({ color: 0x656565 })
+var cylCabineMat = new THREE.MeshPhongMaterial({ color: 0x656565 })
+var sCabineMat = new THREE.MeshPhongMaterial({ color: 0x656565 })
 
 var cylCabine = new THREE.Mesh(cylCabineGeo, cylCabineMat)
 var sCabine1 = new THREE.Mesh(sCabineGeo, sCabineMat)
 var sCabine2 = new THREE.Mesh(sCabineGeo, sCabineMat)
 
 // ----------------- Adicionando os polígonos à cena e colocando-os em suas posições ----------------- //
-
 scene.add(esferaHelice)
-esferaHelice.translateX(0).translateY(10).translateZ(0);
+esferaHelice.translateX(posX).translateY(posY).translateZ(posZ)
+
+//Objeto para auxiliar no movimento
+var esferaMovGeo = new THREE.SphereGeometry(1, 1, 1)
+var esferaMov = new THREE.Mesh(esferaMovGeo, esferaHeliceMat)
+esferaHelice.add(esferaMov)
+esferaHelice.translateX(posX).translateY(posX).translateZ(posZ)
 
 // Cone para colocar as hélices rotacionando
 esferaHelice.add(coneHelice)
@@ -129,7 +132,7 @@ cylHelice.matrix.identity()
 cylHelice.matrix.multiply(mat4.makeTranslation(0.0, 0.0, 1.0 * c))
 
 // Cilindro 1 do corpo do aviao
-esferaHelice.add(cyl1corpo)
+esferaMov.add(cyl1corpo)
 cyl1corpo.matrixAutoUpdate = false
 cyl1corpo.matrix.identity()
 cyl1corpo.matrix.multiply(mat4.makeRotationX(degreesToRadians(90)))
@@ -232,162 +235,203 @@ sCabine1.matrix.multiply(mat4.makeTranslation(0.0, -2.1 * c, 0.0))
 sCabine2.matrix.multiply(mat4.makeTranslation(0.0, 2.1 * c, 0.0))
 
 // ------------------------------ Movimento ---------------------------------
+// Variaveis auxiliares
 var speed = 0
 var posX = 0
-var posY = 0
+var posY = 10
 var posZ = 0
 var angX = 0
 var angY = 0
 var angZ = 0
-
-
-function aceleracao() {
-  if (speed > 0) {
-    esferaHelice.translateZ( speed );
-  }
-
-}
-
-var speedHelice = 0
-function movHelice() {
-  if(animation) {
-    speedHelice+=speed;
-    cylHelice.matrixAutoUpdate = false
-    cylHelice.matrix.multiply(mat4.makeRotationZ(speed))
-  }
-}
-
-var auxRotVertical = 0;
-var auxRotHorizontal = 0;
-
-function nivelamento()
-{
-  if (auxRotVertical > 0) 
-  {
-    setTimeout(function() { esferaHelice.rotateOnAxis(rotX, angle2) }, 1000);
-    auxRotVertical --;
-  }
-
-  if (auxRotVertical < 0) 
-  {
-    setTimeout(function() { esferaHelice.rotateOnAxis(rotX, -angle2) }, 1000);
-    auxRotVertical ++;
-  }
-
-  if (auxRotHorizontal > 0) 
-  {
-    setTimeout(function() { esferaHelice.rotateOnAxis(rotZ, angle) }, 1000);
-    auxRotHorizontal --;
-  }
-  
-  if (auxRotHorizontal < 0) 
-  {
-    setTimeout(function() { esferaHelice.rotateOnAxis(rotZ, -angle) }, 1000);
-    auxRotHorizontal ++;
-  }
-
-}
-
-// ----------------- Camera ----------------- //
-
-var plane = createGroundPlaneWired(500, 500, 50, 50)
-//plane.rotateX(degreesToRadians(-90));
-scene.add (new THREE.HemisphereLight())
-
-var axesHelper = new THREE.AxesHelper( 12 )
-
-var modoCam = true
-var camera = initCamera(new THREE.Vector3(0, 20, -120 ))
-
-var camera2 = initCamera(new THREE.Vector3(100, 20, -120 ))
-var trackballControls = new TrackballControls( camera2, renderer.domElement )
-
-cam1()
-
-function cam1(){
-  scene.add(plane)
-  
-  trackballControls = new TrackballControls( camera2, renderer.domElement )
-  //camera.lookAt(0, -5 , 0);
-  camera.position.set(0, 20, -120);
-  esferaHelice.add(camera)
-  //modoCam = true
-}
-
-function cam2(){
-  scene.add(axesHelper)
-  scene.remove(plane)
-  trackballControls = new TrackballControls( camera, renderer.domElement )
-  //modoCam = false
-}
-//Nivelamento da 
-
-//camera.rotateX(degreesToRadians(10));
-
-
-
-// --------------------------- Keybord---------------------------------- //
-
-var keyboard = new KeyboardState();
 var rotZ = new THREE.Vector3(0,0,1);
 var rotY = new THREE.Vector3(0,1,0);
 var rotX = new THREE.Vector3(1,0,0);
 var angle = degreesToRadians(0.5);
-var angle2 = degreesToRadians(0.3);
+var angle2 = degreesToRadians(0.1);
 
-function keyboardUpdate() {
+// Movimento de aceleracao
+function aceleracao() {
+  if(!modoCam) { // Depende do modo da camera
+    if (speed > 0){
+      esferaHelice.translateZ( speed )
+      cylHelice.matrix.multiply(mat4.makeRotationZ(speed/3)) // Rotação da helice
+    }
+  }
+}
+function acelera() {
+  if(!modoCam) { //Previne continuação de movimento na troca de camera
+    if(speed < 1) {
+      speed += 0.01
+      setTimeout(acelera, 300) // Aceleracao progressiva de modo recursivo
+    }
+  }
+}
+function desacelera() {
+  if(!modoCam) { //Previne continuação de movimento na troca de camera
+      if(speed > 0) {
+      speed -= 0.01
+      setTimeout(desacelera, 300)
+    }
+  }
+}
 
-  keyboard.update()
+// Movimento direcao
+function esquerda() {
+  esferaMov.rotateOnAxis(rotZ, -angle )
+  esferaHelice.rotateOnAxis(rotY, angle2 )
+  auxRotHorizontal ++
+}
+function direita() {
+  esferaMov.rotateOnAxis(rotZ, angle );
+  esferaHelice.rotateOnAxis(rotY, -angle2 )
+  auxRotHorizontal --
+}
+function cima() {
+  esferaHelice.rotateOnAxis(rotX, angle2)
+  esferaCam.rotateOnAxis(rotX, -angle2)
+  auxRotVertical --
+}
+function baixo(){
+  esferaHelice.rotateOnAxis(rotX, -angle2)
+  esferaCam.rotateOnAxis(rotX, angle2)
+  auxRotVertical ++
+}
 
+// Funcao para salvar posicao
+var position = new THREE.Vector3()
+function getPosition() {
+  scene.updateMatrixWorld(true)
+  position.setFromMatrixPosition( esferaHelice.matrixWorld )
+}
 
-  if ( keyboard.pressed("down") ) {
-    //esferaHelice.rotateOnAxis(rotX, -angle2)
-    cyl1corpo.rotateOnAxis(rotZ, -angle2)
-    //auxRotVertical ++;
+// Movimente de nivelamento do aviao
+var auxRotVertical = 0;
+var auxRotHorizontal = 0;
+function nivBaixo() {
+  if (auxRotVertical > 0) {
+    esferaHelice.rotateOnAxis(rotX, angle2)
+    esferaCam.rotateOnAxis(rotX, -angle2)
+    auxRotVertical --
+    setTimeout(nivBaixo, 200)
+  }
+}
+function nivCima() {
+  if (auxRotVertical < 0) {
+    esferaHelice.rotateOnAxis(rotX, -angle2) 
+    esferaCam.rotateOnAxis(rotX, angle2)
+    auxRotVertical ++
+    setTimeout(nivCima, 200)
+  }
+}
+function nivEsq() {
+  if (auxRotHorizontal > 0) {
+    esferaMov.rotateOnAxis(rotZ, angle)
+    auxRotHorizontal --
+    setTimeout(nivEsq, 200)
+  }
+}
+function nivDir() {
+  if (auxRotHorizontal < 0) {
+    esferaMov.rotateOnAxis(rotZ, -angle)
+    auxRotHorizontal ++
+    setTimeout(nivDir, 200)
+  }
+}
+
+// ----------------- Camera ----------------- //
+//Objeto que carregara a camera
+var esferaCamGeo = new THREE.SphereGeometry(1, 1, 1)
+var esferaCam = new THREE.Mesh(esferaCamGeo, esferaHeliceMat)
+esferaHelice.add(esferaCam)
+esferaCam.translateX(0).translateY(0).translateZ(-20)
+
+var axesHelper = new THREE.AxesHelper( 12 )
+var camera = initCamera(new THREE.Vector3(0, 20, -120 ))
+var clear = initCamera(new THREE.Vector3(0,0,0)) // Usada apenas para limpar o trackball
+var trackballControls = new TrackballControls( clear, renderer.domElement )
+var modoCam = true // Auxilia na selecao da camera
+
+esferaCam.add(camera)
+//Funcao para a troca do modo de camera
+switchCam()
+function switchCam(){
+  if(modoCam){
+    scene.add(plane)
+    trackballControls = new TrackballControls( clear, renderer.domElement ) // Remove o trackball
+    esferaHelice.position.set(posX, posY, posZ) // Posiciona na posicao anterior salva
+    camera.position.set(posX, posY+20, posZ-120) // Reposiciona camera
+    //Falta arrumar angulo da camera
+    modoCam = false // Auxilia modo da camera
+  }
+  else {
+    posX = position.x // Salva a posicao
+    posY = position.y
+    posZ = position.z
+    speed = 0 // Interrompe o movimento
+    scene.add(axesHelper)
+    scene.remove(plane)
+    trackballControls = new TrackballControls( camera, renderer.domElement ) // Add trackball
+    esferaHelice.position.set(0,0,25) // Posiciona aviao no centro
+    modoCam = true
   }
 
-  if ( keyboard.pressed("up") ) {
-    esferaHelice.rotateOnAxis(rotX, angle2);
-    auxRotVertical --;  
-  }       
+}
 
-  if ( keyboard.pressed("left") ) {
-    esferaHelice.rotateOnAxis(rotZ, -angle );
-    auxRotHorizontal ++;
-  }     
+// Nivelamento da Camera
+//camera.rotateX(degreesToRadians(10));
 
-  if ( keyboard.pressed("right") ) {
-    esferaHelice.rotateOnAxis(rotZ, angle );
-    auxRotHorizontal --;
-  }  
+// --------------------------- Keybord---------------------------------- //
+
+var keyboard = new KeyboardState();
+
+function keyboardUpdate() {
+  keyboard.update()
+  if(!modoCam){ //Apenas no modo simulacao
+    if ( keyboard.pressed("down") ) {   
+      if (auxRotVertical < 400)       
+        baixo()
+    }
+    else if (auxRotVertical > 0)      
+      nivBaixo()
+
+    if ( keyboard.pressed("up") ) {     
+      if (auxRotVertical > -400)      
+        cima()
+    }
+    else if (auxRotVertical < 0)      
+      nivCima()
+
+    if ( keyboard.pressed("left") )  {
+      if (auxRotHorizontal < 150) 
+        esquerda()
+    }
+    else if (auxRotHorizontal > 0)    
+      nivEsq()
+
+    if ( keyboard.pressed("right") )  {
+      if (auxRotHorizontal > -150) 
+        direita()
+    }
+    else if (auxRotHorizontal < 0)   
+      nivDir()
     
-  if ( keyboard.pressed("Q") )
-    if(speed < 0.5)
-      speed += 0.01
-
-  if ( keyboard.pressed("A") )
-    if(speed > 0)
-        speed -= 0.01    
-
-  if ( keyboard.pressed("S") ) // Comando para trocar modo de camera
-    if(modoCam){
-      cam2()
-      modoCam = false
-    }
-    else {
-      cam1()
-      modoCam = true
-    }
+    if ( keyboard.pressed("Q") )      acelera()
+    if ( keyboard.pressed("A") )      desacelera()
+  }
+  if ( keyboard.pressed("space") )  switchCam()
 }
 
 // Use this to show information onscreen
 var controls = new InfoBox()
-controls.add('Basic Scene')
+controls.add('Use SPACE para mudar o modo')
 controls.addParagraph()
-controls.add('Use mouse to interact:')
-controls.add('* Left button to rotate')
-controls.add('* Right button to translate (pan)')
-controls.add('* Scroll to zoom in/out.')
+controls.add('Modo 1: Simulador')
+controls.add('* Q para acelerar')
+controls.add('* A para desacelerar')
+controls.add('* Setas para direcionar')
+controls.addParagraph()
+controls.add('Modo 2: Visualização')
+controls.add('* Utilize o mouse para rotacionar')
 controls.show()
 
 // Listen window size changes
@@ -401,14 +445,12 @@ window.addEventListener(
 
 render()
 function render() {
-  stats.update() // Update FPS
+  stats.update()
   trackballControls.update()
-  
-  nivelamento()
+  //nivelamento()
   keyboardUpdate()
   aceleracao()
-  movHelice()
-
+  getPosition()
   requestAnimationFrame(render)
   renderer.render(scene, camera) // Render scene
 }
